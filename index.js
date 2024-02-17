a0d993489a29557ccf5ccbc969569a977dc0a8d3
rabbani4u <rabbani.iu31@gmail.com>
2024-02-17 20:25:04 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// let randomNumber = Math.floor(Math.random() * 6) + 1;
+// console.log(randomNumber);
+
+// function isLeapYear(year) {
+//   if (year % 4 == 0 || (year % 400 == 0 && year % 100 != 0)) {
+//     console.log(year + " is LeapYear");
+//   } else {
+//     console.log(year + " is not LeapYear");
+//   }
+// }
+
+// isLeapYear(2021);
+
+let name = ["Rabbani", "Hakil", "Jakil", "Ulio", "Mikeee"];
+console.log(name[2]);
